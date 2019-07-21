6b8d4d344a1d7aee2adac146b5926894b9cb9114
Amit Butala <abutala@gmail.com>
2019-07-21 10:06:34 -0700

    Cleanup the nodecheck script

--- NodeCheck/purge_old_foscam_files.sh
+++ NodeCheck/purge_old_foscam_files.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 ## Note: Do not call directly, use the *.py wrapper to active email alerts. 
+BASEDIR=$(dirname "$0")
+source "$BASEDIR/../lib/Constants.sh"
 
-DIR="/mnt/IPCam_Data"
-PURGE=90                  # Days to keep the files
-DEVICES=5                 # Number of foscams deployed
-KEEP=$(expr $PURGE - 10)  # Min days check
+KEEP=$(expr $PURGE_AFTER_DAYS - 10)  # Min days check
 CUMULATIVE="\n\nBacktrace:\n"
-MAX_FILES_PER_YARD_NODE=75 # Oh noes! cobwebs! 
 
-cd $DIR; ls -al > /dev/null
-df | grep -q $DIR
+cd $FOSCAM_DIR; ls -al > /dev/null
+df | grep -q $FOSCAM_DIR
 if [[ $? -ne 0 ]]; then 
-  >&2 echo "Error: Drive $DIR is not mounted" 
+  >&2 echo "Error: Drive $FOSCAM_DIR is not mounted" 
   exit -1
 else
-  echo "Drive $DIR is mounted"
+  echo "Drive $FOSCAM_DIR is mounted"
 fi
 
 ## TODO: Investigate why ctime does not work. Till then we shall fall back to mtime
-STEP1="Deleting all IPCam data older than $PURGE days..."
+STEP1="Deleting all IPCam data older than $PURGE_AFTER_DAYS days..."
 echo -n "$STEP1"
 # REM forfiles -p "." -s -m *.* /D -%lookback% /C "cmd /c del /s @path" >> logs.txt 2>&1
-OUT1=$(find . -mindepth 2 -type f -mtime +$PURGE -exec rm {} \;)
+OUT1=$(find . -mindepth 2 -type f -mtime +$PURGE_AFTER_DAYS -exec rm {} \;)
 CUMULATIVE="$CUMULATIVE$STEP1\n$OUT1\n"
 echo "Done!"
 
@@ -29,7 +27,7 @@ STEP2="Confirm all devices are logging..."
 echo "$STEP2"
 OUT2=$(find . -mindepth 2 -type f -mtime -1 | cut -d "/" -f 2 | sort | uniq -c | grep "-")
 CUMULATIVE="$CUMULATIVE$STEP2\n$OUT2\n"
-if [[ $(echo "$OUT2" | wc -l) -ne $DEVICES ]]; then 
+if [[ $(echo "$OUT2" | wc -l) -lt $MIN_ACTIVE_DEVICES ]]; then 
   >&2 echo "Error: $STEP2 failed" 
   >&2 echo -en "$CUMULATIVE"
   exit -1
@@ -41,7 +39,7 @@ STEP3="Confirm all devices have atleast $KEEP days of data..."
 echo "$STEP3"
 OUT3=$(find . -mindepth 2 -type f -mtime +$KEEP | cut -d "/" -f 2 | sort | uniq -c | grep "-")
 CUMULATIVE="$CUMULATIVE$STEP3\n$OUT3\n"
-if [[ $(echo "$OUT3" | wc -l) -ne $DEVICES ]]; then 
+if [[ $(echo "$OUT3" | wc -l) -lt $MIN_ACTIVE_DEVICES ]]; then 
   >&2 echo "Error: $STEP3 failed" 
   >&2 echo -en "$CUMULATIVE"
   exit -1
@@ -49,15 +47,15 @@ else
   echo "$OUT3"
 fi
 
-STEP4="Confirm all data beyond $PURGE days is culled..."
+STEP4="Confirm all data beyond $PURGE_AFTER_DAYS days is culled..."
 echo "$STEP4"
-OUT4=$(find . -mindepth 2 -type f -mtime +$(expr $PURGE + 1) | cut -d "/" -f 2 | sort | uniq -c | grep "-")
+OUT4=$(find . -mindepth 2 -type f -mtime +$(expr $PURGE_AFTER_DAYS + 1) | cut -d "/" -f 2 | sort | uniq -c | grep "-")
 RETVAL=$?
 CUMULATIVE="$CUMULATIVE$STEP4\n$OUT4\n"
 if [[ $RETVAL -ne 1 ]]; then 
   >&2 echo "Error: $STEP4 failed" 
   >&2 echo -en "$CUMULATIVE\n"
-  >&2 find . -mindepth 2 -type f -mtime +$PURGE
+  >&2 find . -mindepth 2 -type f -mtime +$PURGE_AFTER_DAYS
   exit -1
 else 
   echo "Success"
@@ -67,7 +65,7 @@ STEP5="Confirm no runaway logging (> $MAX_FILES_PER_YARD_NODE files/day) in the 
 FAILED=0
 echo "$STEP5"
 #OUT5=$(find . -mindepth 2 -type f -mtime -1 | cut -d "/" -f 2 | sort | uniq -c | grep "-")
-OUT5=$(echo "$OUT2" | grep "yard")
+OUT5=$(echo "$OUT2" | grep -i "yard")
 for numFiles in $(echo "$OUT5" | sed "s/^\s*\(\S*\).*$/\1/"); do
   if [[ $numFiles -gt $MAX_FILES_PER_YARD_NODE ]]; then
     FAILED=1
