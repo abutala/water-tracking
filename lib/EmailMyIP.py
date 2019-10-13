8bd05629cf2c3dff011a6d2f06ad55dd05cecb47
Amit Butala <abutala@gmail.com>
2019-10-12 23:15:06 -0700

    Add script for self reporting on IP address change

---
+++ lib/EmailMyIP.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.5
+
+import logging
+import os
+import sys
+import urllib.request
+import Constants
+import Mailer
+
+
+#### Main Routine ####
+if __name__ == "__main__":
+  logfile = '%s/%s.log' % (Constants.LOGGING_DIR, os.path.basename(__file__))
+  log_format = '%(levelname)s:%(module)s.%(lineno)d:%(asctime)s: %(message)s'
+  logging.basicConfig(filename=logfile, format=log_format, level=logging.INFO)
+  logging.info('============')
+  logging.info('Invoked command: %s' % ' '.join(sys.argv))
+
+  IP_PAGE="http://myip.dnsomatic.com/"
+  alert=False
+  msg=""
+  try:
+    page = urllib.request.urlopen(IP_PAGE)
+    msg = page.read().decode("utf-8")
+    logging.debug('got %s' % msg)
+  except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
+    msg = e.output
+    alert = True
+  finally:
+    logging.info(msg)
+
+  Mailer.sendmail(topic="[Eden's IP]", message=msg, always_email=True, alert=alert)
+
