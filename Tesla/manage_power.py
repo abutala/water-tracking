ee76d0086c3260588ad39b546f59e816d8eb3cc2
Amit Butala <abutala@gmail.com>
2022-06-19 23:56:12 -0700

    tighten handling at transitions

--- Tesla/manage_power.py
+++ Tesla/manage_power.py
@@ -8,7 +8,7 @@ import logging
 import os
 import sys
 import time
-from typing import List
+from typing import List, Optional
 import Constants
 import Mailer
 import MyTwilio
@@ -29,39 +29,30 @@ def average(my_list):
   return 0 if not my_list else sum(my_list)/len(my_list)
 
 
-def extrapolate(my_list) -> float:
+def extrapolate(my_list: List[float], time_sampling: float = 1) -> Optional[float]:
   # works only on reverse sorted.
+  if not my_list:
+    return None
   avg_diff = average([my_list[i] - my_list[i+1] for i in range(len(my_list) - 1)])
-  return round(my_list[0] + avg_diff, 2)
+  return round(my_list[0] + avg_diff*time_sampling, 2)
 
 
-def sanitize_pct(pct, historical_pcts: List[float]) -> float:
+def sanitize_pct(pct:float, historical_pcts: List[float], time_sampling: float) -> float:
+  # Doesn't handle dynamicism on time_sampling very well. We should just store
+  # epoch time in historical_pcts
   MAX_HISTORY = 5
+  pct = round(pct, 2)
   updated_pct = pct
-  if pct <= 0 or pct == historical_pcts[0]:
-    updated_pct = round(extrapolate(historical_pcts),2)
-    logging.warning(f"Got bad batt data:{updated_pct}% Patched:{pct:.2f}% from {historical_pcts} and continue..")
+  if len(historical_pcts) >= MAX_HISTORY and (pct <= 0 or pct == historical_pcts[0]):
+    updated_pct = round(extrapolate(historical_pcts, time_sampling),2)
+  if updated_pct != pct:
+    logging.warning(f"Got bad batt data:{pct}% Patched:{updated_pct:.2f}% from {historical_pcts} and continue..")
+  if updated_pct <= 0:
+      raise AssertionError(f"Could not fix bad batt data: {updated_pct}%. Throwing...")
   historical_pcts.insert(0, updated_pct)
   if len(historical_pcts) > MAX_HISTORY:
     historical_pcts.pop()
   return updated_pct
-
-
-def update_powerwall(op_mode, backup_pct, point, product, send_sms):
-  status = status2 = None
-  if op_mode != point.op_mode:
-    status = f"{point.op_mode} "
-    status += product.set_operation(point.op_mode)
-  if backup_pct !=  point.pct_min:
-    status2 = f"{point.pct_min} "
-    status2 += product.set_backup_reserve_percent(int(point.pct_min))
-
-  if status or status2:
-    msg = f"At:{pct}% Rule: {point.reason}" + \
-          f"  Mode:{status or 'No change'}  Reserve %:{status2 or 'No change'}"
-    logging.warning(msg)
-    if send_sms:
-      MyTwilio.sendsms(SMS_RCPT, msg)
 
 
 def main(args):
@@ -71,15 +62,15 @@ def main(args):
       logging.info(f'{product["site_name"]} discovered')
       loop_count = 0
       fail_count = 0
-      historical_pcts = [0]
+      sleep_time = 0
+      historical_pcts = list()
 
       while True:
         loop_count += 1
-        if fail_count > 10:
-          raise AssertionError(f"Continuously failing PW test. Exiting..")
+        time.sleep(sleep_time)
 
-        currtime = time.localtime()
         reload(Constants)
+        currtime = time.localtime()
         SMS_RCPT = Constants.POWERWALL_SMS_RCPT
         POLL_TIME_IN_SECS = Constants.POWERWALL_POLL_TIME
         DECISION_POINTS = Constants.POWERWALL_DECISION_POINTS
@@ -87,22 +78,26 @@ def main(args):
 
         try:
           product.get_battery_data()
+          logging.debug(json.dumps(product))
+
           op_mode = product["operation"]
           backup_pct = product["backup"]["backup_reserve_percent"]
           can_export = product["components"].get("customer_preferred_export_rule", "Not Found")
           can_grid_charge = not product["components"].get("disallow_charge_from_grid_with_solar_installed", False)
           assert (can_export == "battery_ok" and can_grid_charge), f"Error in PW config. Got export: {can_export}, grid_charge: {can_grid_charge}"
 
-          pct = round(product["energy_left"]/product["total_pack_energy"] * 100, 2)
-          pct = sanitize_pct(pct, historical_pcts)
-          future_pct = extrapolate(historical_pcts)
+          pct = product["energy_left"]/product["total_pack_energy"] * 100
+          pct = 0
+          pct = sanitize_pct(pct, historical_pcts, sleep_time/POLL_TIME_IN_SECS)
+          future_pct = extrapolate(historical_pcts) or pct
+          sleep_time = POLL_TIME_IN_SECS
         except Exception as e:
-          logging.warning(f"Powerwall read failed with {e}. Discarding...")
-          logging.debug(f"Got:{product}")
+          logging.warning(f"Failed {fail_count} times with {e}. Discarding...")
+          sleep_time = min(POLL_TIME_IN_SECS, 30) # Quick retry
           fail_count += 1
-          time.sleep(POLL_TIME_IN_SECS)
+          if fail_count > 10:
+            raise AssertionError(f"Continuously failing with {e}. Exiting..")
           continue
-        logging.debug(json.dumps(product))
         logging.info(f"Read %:{pct:.2f}  Mode:{op_mode}  Export:{can_export}  Grid Charge:{can_grid_charge}")
 
         for point in DECISION_POINTS:
@@ -116,31 +111,35 @@ def main(args):
 
             if condition_matches(pct, trigger_now_pct, point.iff_higher):
               # Rule applies. Send command if needed. Do not process further
-              logging.info(f"Matched rule at {pct} for {trigger_now_pct}%: {point}")
-              update_powerwall(op_mode, backup_pct, point, product, args.send_sms)
+              logging.info(f"Matched with {trigger_now_pct}%: {point.reason}")
+              status = f"{point.op_mode} "
+              if op_mode != point.op_mode:
+                status += product.set_operation(point.op_mode)
+              status2 = f"{point.pct_min} "
+              if backup_pct !=  point.pct_min:
+                status2 += product.set_backup_reserve_percent(int(point.pct_min))
+              if status or status2:
+                msg = f"At:{pct}%, {point.reason}  Mode:{status}  Reserve %:{status2}"
+                logging.warning(msg)
+                if args.send_sms:
+                  MyTwilio.sendsms(SMS_RCPT, msg)
               break  # out of for loop
-            elif False and condition_matches(future_pct, trigger_next_pct, point.iff_higher):
-              # Disabled for now...
-              logging.info(f"Matched future at {future_pct} for {trigger_next_pct}%: {point}")
-              extra_sleep = POLL_TIME_IN_SECS * 0.5 ## Needs geometry, just take mid point for now :(
-              logging.warning("Almost at tripping point, let's wait {extra_sleep} secs for it")
-              time.sleep(min(extra_sleep,POLL_TIME_IN_SECS)) # Prevents disaster.
-              # The future is now here
-              update_powerwall(op_mode, backup_pct, point, product, args.send_sms)
+            elif condition_matches(future_pct, trigger_next_pct, point.iff_higher):
+              logging.warning(f"Matched future with {trigger_next_pct}%: {point.reason} Fast retry.. ")
+              sleep_time = min(POLL_TIME_IN_SECS, 30)
               break  # out of for loop
             else:
-              logging.info(f"In time window, but skip: Trig:{trigger_now_pct}% {point}")
+              logging.info(f"In time window, but skip: Trig:{trigger_now_pct}% {point.reason}")
         else:
           logging.warning(f"Matched no rule. Is that okay?")
-
-        # Sleep, then while True loop...
-        time.sleep(POLL_TIME_IN_SECS)
 
   except EnvironmentError as e:
     logging.error(f"Oops. Telsa token expired? Run gui.py direclty from TeslaPy. Error: {e}")
     MyTwilio.sendsms(SMS_RCPT, "Oops. Telsa token expired? Run gui.py direclty from TeslaPy")
   except Exception as e:
+    import traceback
     logging.error(e)
+    logging.error(traceback.print_exc())
     if args.send_sms:
        MyTwilio.sendsms(SMS_RCPT, e.__repr__())
   logging.error(f"Will hard exit after delay of 3600 seconds to prevent respawn churn...\n\n\n\n\n")
